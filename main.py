0c2f3edca20916b8bbe9f7c81a647e29877257f1
Shivam Gupta <shivam.javreva@gmail.com>
2023-06-13 15:30:09 +0530

    option data updated

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 import csvManager
 import dfManager
 import expiryDateManager
@@ -9,10 +11,8 @@ import os
 if __name__ == '__main__':
 
     # initialize Trade log Dataframe
-    trade_log = ['Entry_Datetime', 'Future_Traded_Price', 'Days_to_Expiry', 'Symbol', 'Entry_Price', 'Exit_Price',
-                 'PnL']
-
-
+    trade_log = ['Symbol','Buy-Sell', 'Future_Traded_Price', 'Days_to_Expiry', 'Entry_Datetime', 'Entry_Price','Exit_Datetime',
+                 'Exit_Price', 'PnL']
 
     bnf_future_file_path = constants.BANKNIFTY_FUTURE_2023_PATH
     CM = csvManager.CsvManager()
@@ -42,6 +42,7 @@ if __name__ == '__main__':
     #
     #
     #***************************************************************************************************************
+    trade_log_index = 0
     for file_path in files:
 
         # get FnO Data into Dataframe
@@ -57,19 +58,77 @@ if __name__ == '__main__':
         # this is the dat format like 05JAN23 which will be used to filter option ticker
         # for ex. BANKNIFTY05JAN2323500PE
         expiry_date_in_option_ticker = str(EDM.convert_date(expiry_date))
-        print(expiry_date_in_option_ticker)
-
-        DfManager = dfManager.DfManager()
-        bnf_options_df = DfManager.filter_BnfOptions_data(fno_df, expiry_date_in_option_ticker)
-        bnf_options_five_min_df = DfManager.get_custom_min_ohlc(bnf_options_df, '5Min')
 
         trading_date_temp = datetime.strptime(trading_date, "%d%m%Y")
         trading_date_dd_mm_yyyy = str(trading_date_temp.strftime("%d-%m-%Y"))
 
-        #*****************************************************************************************************
+        # *****************************************************************************************************
         # get banknifty inraday daya from dataframe which has complete year ohlc data
-        #*****************************************************************************************************
+        # *****************************************************************************************************
         banknifty_itraday_df = banknifty_dataframe[(banknifty_dataframe['Date'] == trading_date_dd_mm_yyyy)]
+        banknifty_intraday_high = banknifty_itraday_df['High'].max()
+        banknifty_intraday_low = banknifty_itraday_df['Low'].min()
+
+        DfManager = dfManager.DfManager()
+        bnf_options_df = DfManager.filter_BnfOptions_data(fno_df, expiry_date_in_option_ticker)
+        all_options_tickers = bnf_options_df['Ticker'].unique()
+        intraday_options_tickers = []
+
+        for ticker in all_options_tickers:
+
+            ticker_strike_price = int(ticker[16:21])
+            if ticker_strike_price < banknifty_intraday_high and ticker_strike_price > banknifty_intraday_low:
+                intraday_options_tickers.append(ticker)
+
+        bnf_options_five_min_df = pd.DataFrame(
+            columns=['Ticker', 'Date', 'Open', 'High', 'Low', 'Close', 'Time'])
+
+        #bnf_options_df.to_csv("C://Historical Data//BANKNIFTY_OPTIONS.csv")
+
+        for ticker in intraday_options_tickers:
+            print(ticker)
+            option_unique_ticker_df = bnf_options_df[bnf_options_df['Ticker'] == ticker]
+            unique_ticker_five_min_ohlc = DfManager.get_custom_min_ohlc(option_unique_ticker_df, '5Min')
+            bnf_options_five_min_df = pd.concat([bnf_options_five_min_df,unique_ticker_five_min_ohlc])
+
+
+
+        #bnf_options_five_min_df = DfManager.get_custom_min_ohlc(bnf_options_df, '5Min')
+        #bnf_options_five_min_df.to_csv("C://Historical Data//BANKNIFTY_OPTIONS.csv")
+
+
+
+
+        for index, row in banknifty_itraday_df.iterrows():
+
+            if row['Time'] == constants.FIRST_STRADDLE_TIME:
+                buy_sell = 'Sell'
+                straddle_strike_price = str(constants.BANKNIFTY_BASE*round(row['Open']/constants.BANKNIFTY_BASE))
+                ce_straddle_symbol = 'BANKNIFTY' + expiry_date_in_option_ticker + straddle_strike_price + 'CE.NFO'
+                pe_straddle_symbol = 'BANKNIFTY' + expiry_date_in_option_ticker + straddle_strike_price + 'PE.NFO'
+                ce_entry_set = False
+                pe_entry_set = False
+                ce_straddle_entry_price = 0
+                pe_straddle_entry_price = 0
+
+                for index, bnf_options in bnf_options_five_min_df.iterrows():
+                    if bnf_options['Ticker'] == ce_straddle_symbol and bnf_options['Time'] == constants.FIRST_STRADDLE_TIME:
+                        ce_straddle_entry_price = bnf_options['Open']
+                        ce_entry_set = True
+
+                    if bnf_options['Ticker'] == pe_straddle_symbol and bnf_options['Time'] == constants.FIRST_STRADDLE_TIME:
+                        pe_straddle_entry_price = bnf_options['Open']
+                        pe_entry_set = True
+
+                    if ce_entry_set == True and pe_entry_set == True:
+                        break
+
+                print(ce_straddle_entry_price)
+                print(pe_straddle_entry_price)
+                print(ce_straddle_symbol)
+                print(pe_straddle_symbol)
+
+
 
 
         break
